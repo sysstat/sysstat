2a2382d15f902223e53a8879c8f8f9bb71c2087b
seb <seb@kluane.home>
2012-09-06 21:07:44 +0200

    Cosmetic change in sadf -H output.
    Replaced "file created using sa/sadc from sysstat version..." with
    "file created by sa/sadc from sysstat version...".

--- sa_common.c
+++ sa_common.c
@@ -943,7 +943,7 @@ int sa_fread(int ifd, void *buffer, int size, int mode)
  */
 void display_sa_file_version(FILE *st, struct file_magic *file_magic)
 {
-	fprintf(st, _("File created using sar/sadc from sysstat version %d.%d.%d"),
+	fprintf(st, _("File created by sar/sadc from sysstat version %d.%d.%d"),
 		file_magic->sysstat_version,
 		file_magic->sysstat_patchlevel,
 		file_magic->sysstat_sublevel);
