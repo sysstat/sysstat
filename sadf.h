e019d978aff25b9b30a86863625120dd32148a47
Sebastien GODARD <sysstat@users.noreply.github.com>
2015-04-13 08:22:55 +0200

    Update DTD and XSD documents
    Update DTD and XSD documents to take into account FC/HBA XML data.
    
    Signed-off-by: Sebastien GODARD <sysstat@users.noreply.github.com>

--- sadf.h
+++ sadf.h
@@ -9,7 +9,7 @@
 #include "sa.h"
 
 /* DTD version for XML output */
-#define XML_DTD_VERSION	"3.0"
+#define XML_DTD_VERSION	"3.1"
 
 /* Possible actions for functions used to display reports */
 #define F_BEGIN	0x01
