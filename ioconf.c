5cc86f47c5af351d1a4dea3835b7d4e428e3fbe1
Sebastien GODARD <sysstat@users.noreply.github.com>
2015-07-13 17:13:11 +0200

    ioconf.c: Fix several ressource leaks
    Some pointers were sometimes not freed, resulting in ressource leaks.
    This patch fixes that.
    
    CID #29711 #29712 #29713.
    
    Signed-off-by: Sebastien GODARD <sysstat@users.noreply.github.com>

--- ioconf.c
+++ ioconf.c
@@ -352,6 +352,18 @@ int ioc_init(void)
 	ioc_parsed = 1;
 
 	return (count);
+
+free_and_return:
+	/* Free pointers and return */
+	fclose(fp);
+	if (blkp) {
+		free(blkp);
+	}
+	if (iocp) {
+		free(iocp);
+	}
+
+	return 0;
 }
 
 /*
