c77a9ddc61c00b4a9a383fa278fa5b418faf1ff9
beejeebus <justin.p.randell@gmail.com>
2015-08-03 13:00:48 -0400

    Adjust fields captured by when scanning /proc/net/snmp for ICMP stats. Fixes #77.

--- rd_stats.c
+++ rd_stats.c
@@ -1240,9 +1240,9 @@ void read_net_icmp(struct stats_net_icmp *st_net_icmp)
 
 		if (!strncmp(line, "Icmp:", 5)) {
 			if (sw) {
-				sscanf(line + 5, "%lu %*u %*u %*u %*u %*u %*u "
-				       "%lu %lu %lu %lu %lu %lu %lu %*u %*u %*u %*u "
-				       "%*u %*u %lu %lu %lu %lu %lu %lu",
+				sscanf(line + 5, "%lu %*u %*u %*u %*u %*u %*u %*u "   // 1 - 8,   capture 1
+				       "%lu %lu %lu %lu %lu %lu %lu %*u %*u %*u %*u " // 9 - 19,  capture 9,10,11,12,13,14,15
+				       "%*u %*u %lu %lu %lu %lu %lu %lu",             // 20 - 27, capture 22,23,24,25,26,27
 				       &st_net_icmp->InMsgs,
 				       &st_net_icmp->InEchos,
 				       &st_net_icmp->InEchoReps,
