sysstat/sysstat

commit 3c6fffdddbe94e96f351257941d6399378346973

Stricter check for file_magic->header_size
The file_magic->header_size should never be outside of the boundary set by
MIN_FILE_HEADER_SIZE and MAX_FILE_HEADER_SIZE.

Should resolve coverity CID 29717 and CID 29719.

sa_common.c
@@ -1274,7 +1274,8 @@ int sa_open_read_magic(int *fd, char *dfile, struct file_magic *file_magic,
 	if ((n != FILE_MAGIC_SIZE) ||
 	    (file_magic->sysstat_magic != SYSSTAT_MAGIC) ||
 	    ((file_magic->format_magic != FORMAT_MAGIC) && !ignore) ||
-	    ((file_magic->header_size > MAX_FILE_HEADER_SIZE) && !ignore) ||
+	    (file_magic->header_size < MIN_FILE_HEADER_SIZE) ||
+	    (file_magic->header_size > MAX_FILE_HEADER_SIZE) ||
 	    ((file_magic->header_size < FILE_HEADER_SIZE) && !ignore)) {
 		/* Display error message and exit */
 		handle_invalid_sa_file(fd, file_magic, dfile, n);

sa.h
@@ -434,7 +434,8 @@ struct file_header {
 };
 
 #define FILE_HEADER_SIZE	(sizeof(struct file_header))
-/* The value below is used for sanity check */
+/* The values below are used for sanity check */
+#define MIN_FILE_HEADER_SIZE	0
 #define MAX_FILE_HEADER_SIZE	8192
 
 

commit 29312d8e24fcece0bfbdd36b61ad6202f66d3630

Fix #124: Conversion from older formats is broken
Commit 3c6fffd introduced a stricter check on file_magic->header_size
field. Yet this field exists only for files created with sysstat
versions 10.3.1 and later and so, should not be checked for older
versions. Trying to check it with older versions breaks the possibility
to convert (using sadf -c) a datafile created by such an old version.

Reported-by: @almosthuman

Signed-off-by: Sebastien GODARD <sysstat@users.noreply.github.com>

sa_common.c
@@ -1274,12 +1274,19 @@ int sa_open_read_magic(int *fd, char *dfile, struct file_magic *file_magic,
 
 	if ((n != FILE_MAGIC_SIZE) ||
 	    (file_magic->sysstat_magic != SYSSTAT_MAGIC) ||
-	    ((file_magic->format_magic != FORMAT_MAGIC) && !ignore) ||
-	    (file_magic->header_size <= MIN_FILE_HEADER_SIZE) ||
-	    (file_magic->header_size > MAX_FILE_HEADER_SIZE) ||
-	    ((file_magic->header_size < FILE_HEADER_SIZE) && !ignore)) {
+	    ((file_magic->format_magic != FORMAT_MAGIC) && !ignore)) {
 		/* Display error message and exit */
 		handle_invalid_sa_file(fd, file_magic, dfile, n);
+	}
+	if ((file_magic->sysstat_version > 10) ||
+	    ((file_magic->sysstat_version == 10) && (file_magic->sysstat_patchlevel >= 3))) {
+		/* header_size field exists only for sysstat versions 10.3.1 and later */
+		if ((file_magic->header_size <= MIN_FILE_HEADER_SIZE) ||
+		    (file_magic->header_size > MAX_FILE_HEADER_SIZE) ||
+		    ((file_magic->header_size < FILE_HEADER_SIZE) && !ignore)) {
+			/* Display error message and exit */
+			handle_invalid_sa_file(fd, file_magic, dfile, n);
+		}
 	}
 	if (file_magic->format_magic != FORMAT_MAGIC)
 		/* This is an old sa datafile format */