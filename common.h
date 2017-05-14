sysstat/sysstat

commit 569378eb1a3be23cdb45ac5d39e354683a7748f8

Fix #148: ARM: sadc crashes because of unaligned memory accesses
Use posix_memalign() with 16 bytes alignment request instead of
realloc() to guarantee 16 byte alignment requested by several
sysstat's structures.

Note: 16 byte alignment is no longer needed by sysstat's
structures. Yet removing the corresponding attributes
(__attribute__ ((aligned (16)))) for example in rd_stats.h
would imply a format change for sar/sadc binary data files.
These attributes will be removed when work is done to make sa binary
datafiles portable (see #135).

Signed-off-by: Sebastien GODARD <sysstat@users.noreply.github.com>

common.h
@@ -11,6 +11,7 @@
 
 #include <time.h>
 #include <sched.h>	/* For __CPU_SETSIZE */
+#include <stdlib.h>
 #include <limits.h>
 
 #ifdef HAVE_SYS_SYSMACROS_H
@@ -114,16 +115,21 @@
 
 /* Allocate and init structure */
 #define SREALLOC(S, TYPE, SIZE)	do {								 \
-   					TYPE *_p_;						 \
-				   	_p_ = S;						 \
+					TYPE *_p_ = S;						 \
    				   	if (SIZE) {						 \
-   				      		if ((S = (TYPE *) realloc(S, (SIZE))) == NULL) { \
+						void *_ptr = NULL;				 \
+						int error = posix_memalign(&_ptr, 16, (SIZE));	 \
+						if (error || (_ptr == NULL)) {			 \
 				         		perror("realloc");			 \
 				         		exit(4);				 \
 				      		}						 \
+						S = (TYPE *)_ptr;				 \
 				      		/* If the ptr was null, then it's a malloc() */	 \
 						if (!_p_) {					 \
 							memset(S, 0, (SIZE));			 \
+						}						 \
+						else {						 \
+							memcpy(S, _p_, (SIZE));			 \
 						}						 \
 				   	}							 \
 					if (!S) {						 \

commit 3c553ec126fa32f126ad1526da8a6e733f2bfd7d

Fix CID 144609: Ressource leak
In SREALLOC() macro: Old pointer was not freed when a realloc() had
happened.
Bug introduced by commit 569378e.

Signed-off-by: Sebastien GODARD <sysstat@users.noreply.github.com>

common.h
@@ -130,6 +130,7 @@
 						}						 \
 						else {						 \
 							memcpy(S, _p_, (SIZE));			 \
+							free(_p_);				 \
 						}						 \
 				   	}							 \
 					if (!S) {						 \