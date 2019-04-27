sysstat/sysstat

commit 5adec60758ccc92fa1477e42d545d4820ba4ae56

iostat, mpstat: Fix JSON output when stopped by SIGINT
Remove extra comma in JSON output displayed by iostat and mpstat when
stopped by SIGINT.

Signed-off-by: Sebastien GODARD <sysstat@users.noreply.github.com>

iostat.c
@@ -1900,12 +1900,6 @@ void rw_io_stat_loop(long int count, struct tm *rectime)
 			if (count > 0) {
 				count--;
 			}
-			if (DISPLAY_JSON_OUTPUT(flags)) {
-				if (count) {
-				printf(",");
-				}
-				printf("\n");
-			}
 		}
 		else {
 			skip = 0;
@@ -1918,9 +1912,12 @@ void rw_io_stat_loop(long int count, struct tm *rectime)
 			if (sigint_caught) {
 				/* SIGINT signal caught => Terminate JSON output properly */
 				count = 0;
-				printf("\n");	/* Skip "^C" displayed on screen */
+			}
+			else if (DISPLAY_JSON_OUTPUT(flags) && count) {
+				printf(",");
 			}
 		}
+		printf("\n");
 	}
 	while (count);
 

mpstat.c
@@ -1930,17 +1930,16 @@ void rw_mpstat_loop(int dis_hdr, int rows)
 
 		if (count) {
 
-			if (DISPLAY_JSON_OUTPUT(flags)) {
-				printf(",\n");
-			}
 			pause();
 
 			if (sigint_caught) {
 				/* SIGINT signal caught => Display average stats */
 				count = 0;
-				printf("\n");	/* Skip "^C" displayed on screen */
 			}
 			else {
+				if (DISPLAY_JSON_OUTPUT(flags)) {
+					printf(",\n");
+				}
 				curr ^= 1;
 			}
 		}

commit f4d4447ce2347def82f9dbfc90d08a5fbbd8b22f

Fix #222: Invalid JSON output displayed by iostat
Commit 5adec60 introduced a regression: JSON output was no longer valid
when option -y was used with iostat.

Signed-off-by: Sebastien GODARD <sysstat@users.noreply.github.com>

iostat.c
@@ -1918,9 +1918,6 @@ void rw_io_stat_loop(long int count, struct tm *rectime, int iodev_nr, int dlist
 				count--;
 			}
 		}
-		else {
-			skip = 0;
-		}
 
 		if (count) {
 			curr ^= 1;
@@ -1930,9 +1927,10 @@ void rw_io_stat_loop(long int count, struct tm *rectime, int iodev_nr, int dlist
 				/* SIGINT signal caught => Terminate JSON output properly */
 				count = 0;
 			}
-			else if (DISPLAY_JSON_OUTPUT(flags) && count) {
+			else if (DISPLAY_JSON_OUTPUT(flags) && count && !skip) {
 				printf(",");
 			}
+			skip = 0;
 		}
 		printf("\n");
 	}