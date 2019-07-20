35510bbf46cf26cabbe94b4e2a4f28d96c44319a
Sebastien GODARD <sysstat@users.noreply.github.com>
2019-07-20 10:22:01 +0200

    Increase array size in get_devname_from_sysfs() function
    Array might be too small in test mode. So increase its size.
    
    Signed-off-by: Sebastien GODARD <sysstat@users.noreply.github.com>

--- sa_common.c
+++ sa_common.c
@@ -538,11 +538,11 @@ void reallocate_all_buffers(struct activity *a, __nr_t nr_min)
   */
 char *get_devname_from_sysfs(unsigned int major, unsigned int minor)
 {
-	static char link[32], target[PATH_MAX];
+	static char link[256], target[PATH_MAX];
 	char *devname;
 	ssize_t r;
 
-	snprintf(link, 32, "%s/%u:%u", SYSFS_DEV_BLOCK, major, minor);
+	snprintf(link, 256, "%s/%u:%u", SYSFS_DEV_BLOCK, major, minor);
 
 	/* Get full path to device knowing its major and minor numbers */
 	r = readlink(link, target, PATH_MAX);
