894a5dab56a88ef90cace1ed4d75d11cd213aa60
Sebastien GODARD <sysstat@users.noreply.github.com>
2020-04-25 09:35:38 +0200

    sar: Don't check if we are close enough to selected interval when interval=1
    Signed-off-by: Sebastien GODARD <sysstat@users.noreply.github.com>

--- sa_common.c
+++ sa_common.c
@@ -618,7 +618,7 @@ char *get_devname(unsigned int major, unsigned int minor, int pretty)
  * @interval	Interval of time.
  *
  * RETURNS:
- * 1 if we are actually close enough to desired interval, 0 otherwise.
+ * TRUE if we are actually close enough to desired interval, FALSE otherwise.
  ***************************************************************************
 */
 int next_slice(unsigned long long uptime_ref, unsigned long long uptime,
@@ -642,6 +642,10 @@ int next_slice(unsigned long long uptime_ref, unsigned long long uptime,
 	}
 
 	last_uptime = uptime;
+
+	if (interval == 1)
+		/* Smallest time interval: Always close enough to desired interval */
+		return TRUE;
 
 	/*
 	 * A few notes about the "algorithm" used here to display selected entries
