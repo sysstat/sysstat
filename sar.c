ad6935db088d6babc4243e8f4a1c7c4ed0e59979
Tim Gates <tim.gates@iress.com>
2020-11-28 10:08:10 +1100

    docs: fix simple typo, stuctures -> structures
    There is a small typo in sar.c, tests/12.0.1/inisar.c.
    
    Should read `structures` rather than `stuctures`.
    
    Signed-off-by: Sebastien GODARD <sysstat@users.noreply.github.com>

--- sar.c
+++ sar.c
@@ -1592,7 +1592,7 @@ int main(int argc, char **argv)
 		/* Read stats from file */
 		read_stats_from_file(from_file);
 
-		/* Free stuctures and activity bitmaps */
+		/* Free structures and activity bitmaps */
 		free_bitmaps(act);
 		free_structures(act);
 
