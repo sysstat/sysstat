e01c8995e62c2e0f00cbb6ca71f2929ae3fb27e9
Sebastien GODARD <sysstat@users.noreply.github.com>
2022-02-13 09:53:22 +0100

    sar/sadf: Don't reallocate buffers for activities not present in file
    Buffer reallocation would fail for activities which are not in the file
    being read. This is because @nr2 = -1 and @nr2 is used to compute the
    memory to allocate in reallocate_all_buffers():
    
    SREALLOC(a->buf[j], void,
    	 (size_t) a->msize * nr_realloc * (size_t) a->nr2);
    
    Signed-off-by: Sebastien GODARD <sysstat@users.noreply.github.com>

--- sa_common.c
+++ sa_common.c
@@ -2998,7 +2998,7 @@ int print_special_record(struct record_header *record_hdr, uint64_t l_flags,
 		 * not been collected in file (or if it has an unknown format).
 		 */
 		for (p = 0; p < NR_ACT; p++) {
-			if (HAS_PERSISTENT_VALUES(act[p]->options)) {
+			if (HAS_PERSISTENT_VALUES(act[p]->options) && (act[p]->nr_ini > 0)) {
 				act[p]->nr_ini = file_hdr->sa_cpu_nr;
 				if (act[p]->nr_ini > act[p]->nr_allocated) {
 					reallocate_all_buffers(act[p], act[p]->nr_ini);
